0f3f360e69a9cc385675a56d4874c5aa7951d59d
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-12 16:30:41 -0400

    se implementa metodo index en TransactionSellerController para obtener el vendedor de una transaccion

--- routes/api.php
+++ routes/api.php
@@ -37,6 +37,7 @@ Route::resource('products', 'Product\ProductController', ['only' =>['index', 'sh
  */
 Route::resource('transactions', 'Transaction\TransactionController', ['only' =>['index', 'show']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' =>['index']]);
+Route::resource('transactions.sellers', 'Transaction\TransactionSellerController', ['only' =>['index']]);
 
 /**
  * Sellers
