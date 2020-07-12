90794aebfedbcb4e88393c07aa2907e4b247fb0d
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-12 17:43:13 -0400

    se implementa el metodo index en SellerProductController para mostrar todos los productos de un vendedor

--- routes/api.php
+++ routes/api.php
@@ -54,6 +54,7 @@ Route::resource('sellers', 'Seller\SellerController', ['only' =>['index', 'show'
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' =>['index']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' =>['index']]);
 Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' =>['index']]);
+Route::resource('sellers.products', 'Seller\SellerProductController', ['except' =>['create', 'show', 'edit']]);
 
 /**
  * Users
