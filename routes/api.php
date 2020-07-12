08b65abde5d359e2f30b54e415ead0edb7aa2926
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-12 17:13:46 -0400

    se implementa el metodo index en CategoryTransactionController para obtener todas las transaciones de una categoria

--- routes/api.php
+++ routes/api.php
@@ -32,6 +32,7 @@ Route::resource('buyers.categories', 'Buyer\BuyerCategoryController', ['only' =>
 Route::resource('categories', 'Category\CategoryController', ['except' =>['create', 'edit']]);
 Route::resource('categories.products', 'Category\CategoryProductController', ['only' =>['index']]);
 Route::resource('categories.sellers', 'Category\CategorySellerController', ['only' =>['index']]);
+Route::resource('categories.transactions', 'Category\CategoryTransactionController', ['only' =>['index']]);
 
 /**
  * Products
