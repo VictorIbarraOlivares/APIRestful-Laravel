e6cdee45fa85b7d79c26b09fb604027cf885e13a
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-12 18:15:25 -0400

    se implementa el metodo destroy en SellerProductController para eliminar un producto que pertenece a un vendedor

--- app/Http/Controllers/Seller/SellerProductController.php
+++ app/Http/Controllers/Seller/SellerProductController.php
@@ -8,6 +8,7 @@ use App\User;
 use App\Seller;
 use App\Product;
 use Illuminate\Http\Request;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class SellerProductController extends ApiController
 {
@@ -71,9 +72,7 @@ class SellerProductController extends ApiController
 
         $this->validate($request, $rules);
 
-        if ( $seller->id != $product->seller_id ) {
-            return $this->errorResponse('El vendedor especificado no es el vendedor real del producto', 422);
-        }
+        $this->verificarVendedor($seller, $product);
 
         $product->fill($request->only([
             'name',
@@ -104,8 +103,19 @@ class SellerProductController extends ApiController
      * @param  \App\Seller  $seller
      * @return \Illuminate\Http\Response
      */
-    public function destroy(Seller $seller)
+    public function destroy(Seller $seller, Product $product)
     {
+        $this->verificarVendedor($seller, $product);
         
+        $product->delete();
+
+        return $this->showOne($product);
+    }
+
+    protected function verificarVendedor(Seller $seller, Product $product)
+    {
+        if ( $seller->id != $product->seller_id ) {
+            throw new HttpException(422, 'El vendedor especificado no es el vendedor real del producto');
+        }
     }
 }
