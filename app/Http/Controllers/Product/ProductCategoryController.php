9d2e6a1a33ffb09f47942b5f4452c50318d939e5
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-12 20:18:30 -0400

    se implementa metodo index en ProductCategoryController para mostrar las categorias de un producto

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\ApiController;
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+        return $this->showAll($categories);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Product $product)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Product  $product
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Product $product)
+    {
+        //
+    }
+}
