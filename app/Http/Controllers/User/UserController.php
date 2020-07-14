d762afae723de5dc38452581c2334b614cbd1dc8
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-13 21:04:51 -0400

    se implementa metodo para reenviar el correo de verificación de la cuenta

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\User;
 
 use App\User;
-use App\Http\Controllers\ApiController;
+use App\Mail\UserCreated;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Mail;
+use App\Http\Controllers\ApiController;
+
 
 class UserController extends ApiController
 {
@@ -135,5 +138,16 @@ class UserController extends ApiController
         $user->save();
 
         return $this->showMessage('La cuenta ha sido verificada');
+    }
+
+    public function resend(User $user)
+    {
+        if ( $user->esVerificado() ) {
+            return $this->errorResponse('Este usuario ya ha sido verificado', 409);
+        }
+
+        Mail::to($user->email)->send( new UserCreated($user) );
+
+        return $this->showMessage('El correo de verficación se ha reenviado');
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -66,3 +66,4 @@ Route::resource('sellers.products', 'Seller\SellerProductController', ['except' 
  */
 Route::resource('users', 'User\UserController', ['except' =>['create', 'edit']]);
 Route::name('verify')->get('users/verify/{token}', 'User\UserController@verify');
+Route::name('resend')->get('users/{user}/resend', 'User\UserController@resend');
