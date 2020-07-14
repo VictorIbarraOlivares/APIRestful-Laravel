931d50fb968102b70cabeb4d431060af430000c6
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-14 19:31:32 -0400

    se crea middleware para incorporar nombre de la aplicación en las respuestas a peticiones http

---
+++ app/Http/Middleware/SignatureMiddleware.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class SignatureMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next, $header = 'X-Name')
+    {
+        $response = $next($request);
+
+        // agrega nombre a cabecera
+        $response->headers->set($header, config('app.name'));
+
+        return $response;
+    }
+}
