00330286895fe770ab38736b771619583f7fc610
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-14 20:05:31 -0400

    se crea la transformación para el modelo user

---
+++ app/Transformers/UserTransformer.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Transformers;
+
+use App\User;
+use League\Fractal\TransformerAbstract;
+
+class UserTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(User $user)
+    {
+        return [
+            'identificador' => (int)$user->id,
+            'nnombre' => (string)$user->name,
+            'correo' => (string)$user->verified,
+            'esVerificado' =>  (int)$user->verfied,
+            'esAdministrador' => ($user->admin === 'true'), // esto es por como viene en el dato de la bd
+            'fechaCreacion' => (string)$user->created_at,
+            'fechaActualizacion' => (string)$user->updated_at,
+            'fechaEliminacion' => isset($user->deleted_at) ? (string) $user->deleted_at : null,
+        ];
+    }
+}
