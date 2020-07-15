b49c2e658147a32eb714e5a3c1d3b07b3db9d723
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-14 20:26:43 -0400

    se implementa transformación para el modelo product

---
+++ app/Transformers/ProductTransformer.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Product;
+use League\Fractal\TransformerAbstract;
+
+class ProductTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Product $product)
+    {
+        return [
+            'identificador' => (int)$product->id,
+            'titulo' => (string)$product->name,
+            'detalles' => (string)$product->description,
+            'disponibles' => (string)$product->quantity,
+            'estado' => (string)$product->status,
+            'imagen' => url("img/{$product->image}"),
+            'vendedor' => (int)$product->seller_id,
+            'fechaCreacion' => (string)$product->created_at,
+            'fechaActualizacion' => (string)$product->updated_at,
+            'fechaEliminacion' => isset($product->deleted_at) ? (string) $product->deleted_at : null,
+        ];
+    }
+}
