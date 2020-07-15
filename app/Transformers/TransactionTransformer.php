1be1fb45de3206a0821653883b94530acf74dd9c
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-14 20:23:00 -0400

    se implementa transformación para el modelo transaction

---
+++ app/Transformers/TransactionTransformer.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace App\Transformers;
+
+use App\Transaction;
+use League\Fractal\TransformerAbstract;
+
+class TransactionTransformer extends TransformerAbstract
+{
+    /**
+     * List of resources to automatically include
+     *
+     * @var array
+     */
+    protected $defaultIncludes = [
+        //
+    ];
+    
+    /**
+     * List of resources possible to include
+     *
+     * @var array
+     */
+    protected $availableIncludes = [
+        //
+    ];
+    
+    /**
+     * A Fractal transformer.
+     *
+     * @return array
+     */
+    public function transform(Transaction $transaction)
+    {
+        return [
+            'identificador' => (int)$transaction->id,
+
+            'cantidad' => (int)$transaction->quantity,
+            'comprador' => (string)$transaction->buyer_id,
+            'producto' => (string)$transaction->product_id,
+
+            'fechaCreacion' => (string)$transaction->created_at,
+            'fechaActualizacion' => (string)$transaction->updated_at,
+            'fechaEliminacion' => isset($transaction->deleted_at) ? (string) $transaction->deleted_at : null,
+        ];
+    }
+}
