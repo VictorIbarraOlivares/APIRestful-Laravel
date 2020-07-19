ebb84c62c16351f7e7fdd342e04d5bb160d17512
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-19 19:32:48 -0400

    restringiendo las acciones que utilizan scope 'manage-account'

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -17,6 +17,7 @@ class UserController extends ApiController
         $this->middleware('client.credentials')->only(['store', 'resend']);
         $this->middleware('auth:api')->except(['store', 'verofy', 'resend']);
         $this->middleware('transform.input:'. UserTransformer::class)->only(['store','update']);
+        $this->middleware('scope:manage-account')->only(['show', 'update']);
     }
 
     /**
