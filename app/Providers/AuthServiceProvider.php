7d365ae1789c0f4213ebce1685e851ed74669427
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-18 22:58:57 -0400

    utilizando el grand type implicito

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -30,5 +30,6 @@ class AuthServiceProvider extends ServiceProvider
         Passport::routes();
         Passport::tokensExpireIn( Carbon::now()->addMinutes(30) ); // tiempo que dura el token, es de 30 minutos
         Passport::refreshTokensExpireIn( Carbon::now()->addDays(30) ); // tiempo limite para refrescar el token, es de 30 días
+        Passport::enableImplicitGrant(); // para obtener tokens de esta manera
     }
 }
