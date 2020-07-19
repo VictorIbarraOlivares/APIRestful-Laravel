d547867cc06ac60d6f7dba9da2a1d2fa6dcfe5e5
VictorIbarraOlivares <victor.ibarra.olivares@gmail.com>
2020-07-18 22:09:28 -0400

    utilizando el componente para gestionar tokens personales

--- resources/js/app.js
+++ resources/js/app.js
@@ -21,6 +21,8 @@ window.Vue = require('vue');
 
 Vue.component('example-component', require('./components/ExampleComponent.vue').default);
 
+Vue.component('passport-personal-access-tokens-component', require('./components/passport/PersonalAccessTokens.vue').default);
+
 /**
  * Next, we will create a fresh Vue application instance and attach it to
  * the page. Then, you may begin adding components to this application
---
+++ resources/views/home/personal-tokens.blade.php
@@ -0,0 +1,9 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <passport-personal-access-tokens-component></passport-personal-access-tokens-component>
+    </div>
+</div>
+@endsection
